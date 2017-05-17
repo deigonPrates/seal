b37b383a8b85bdcb57d216df36a964b851f54217
Deigon <deigon_prates@hotmail.com>
2017-05-16 21:52:09 -0300

    corrigindo erro de ortografia

--- paginas/cadastrar/adicionarQuestao.php
+++ paginas/cadastrar/adicionarQuestao.php
@@ -111,7 +111,7 @@ $conexao->BDFecharConexao($con);
                         <div class="form-group">
                             <div class="col-sm-offset-5 col-sm-9">
                                 <button type="submit" class="btn btn-success waves-effect waves-light">Cadastrar</button>
-                                <button type="button" class="btn btn-danger waves-effect waves-light"><a href="/inicio" style="color: white;"> Canselar</a></button>
+                                <button type="button" class="btn btn-danger waves-effect waves-light"><a href="/inicio" style="color: white;"> Cancelar</a></button>
                             </div>
                         </div>
                     </div>
