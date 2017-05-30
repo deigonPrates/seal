7aa7bcf45906cd9b6309bca84fb3c8d2ecee7504
Deigon <deigon_prates@hotmail.com>
2017-05-29 23:08:14 -0300

    adicionando liks para o listar do aluno

--- headerAluno.php
+++ headerAluno.php
@@ -118,9 +118,9 @@ $conn->BDFecharConexao($conexao);
                             <li class="has_sub">
                                 <a href="#" class="waves-effect"><i class="ti-light-bulb"></i><span>Listar</span></a>
                                 <ul class="list-unstyled">
-                                    <li><a href="/listar/atividade">Atividades</a></li>
-                                    <li><a href="/listar/avaliacao">Avaliações</a></li>
-                                    <li><a href="/listar/turma">Turmas</a></li>
+                                    <li><a href="/listar/atividades">Atividades</a></li>
+                                    <li><a href="/listar/avaliacoes">Avaliações</a></li>
+                                    <li><a href="/listar/turmas">Turmas</a></li>
                                 </ul>
                             </li>
                             <li class="has_sub">
