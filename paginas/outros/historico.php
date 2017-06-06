427c3e7c29b5f7987b3adc00ba3a751e4e4ccb34
Deigon <deigon_prates@hotmail.com>
2017-06-06 00:03:53 -0300

    criando a visualização do historico pelos alunos

--- paginas/outros/historico.php
+++ paginas/outros/historico.php
@@ -7,80 +7,99 @@ if (!isset($_SESSION['matricula'])) {
 include("./assets/mpdf/mpdf.php");
 include_once("./classes/conexao.class.php");
 
-$con = new Conexao();
-$conexao = $con->BDAbreConexao();
-$matricula = $_SESSION['matricula'];
-$id = $con->BDRetornaID($matricula);
-$tabela = $con->BDRetornarTabela($matricula);
-$consulta = $con->BDSeleciona($tabela, '*', "WHERE(id = $id)");
-$consulta_banco = $con->BDSQL("SELECT turmas.nome, turmas.ano, turmas.semestre from alunos
-                                join registros on registros.aluno_id = alunos.id
-                                JOIN turmas on registros.turma_id = turmas.id
-                                WHERE(alunos.id = $id)");
-$consultabd = $con->BDSQL("SELECT sum(questoes.valor) as nota, atividades.conteudo, atividades.dataInicio from alunos "
+$conexao = new Conexao();
+$con = $conexao->BDAbreConexao();
+
+$alunos = $conexao->BDSeleciona('alunos', 'alunos.nome, alunos.email,alunos.matricula', "WHERE(id = 1)");
+$turmas = $conexao->BDSeleciona('turmas', 'turmas.ano, turmas.semestre', "WHERE(id = 1)");
+$consultabd = $conexao->BDSQL("SELECT sum(questoes.valor) as nota, atividades.conteudo, atividades.dataInicio from alunos "
         . "join alunos_atividades on alunos.id = alunos_atividades.aluno_id "
         . "JOIN atividades on atividades.id = alunos_atividades.avaliacao_id "
         . "JOIN questoes on atividades.id = questoes.atividade_id "
         . "join respostas on questoes.id = respostas.questao_id "
-        . "WHERE(respostas.resultado = 1 and alunos.id = $id) "
+        . "WHERE(respostas.resultado = 1 and alunos.id = 1) "
         . "GROUP by atividades.conteudo, atividades.dataInicio");
-$con->BDFecharConexao($conexao);
+$conexao->BDFecharConexao($con);
 
-$nome = $consulta[0]['nome'];
-$ano = $consulta_banco[0]['ano'] . '.' . $consulta_banco[0]['semestre'];
-$turma = $consulta_banco[0]['nome'];
-
-$laco = null;
-$cont = 1;
-foreach ($consultabd as $key => $value) {
-    $laco .= "<tr>";
-    $laco .= "<td>{$cont}</td>";
-    $laco .= "<td>{$value['conteudo']}</td>";
-    $laco .= "<td>{$value['nota']}</td>";
-    $laco .= "<td>{$value['dataInicio']}</td>";
-    $laco .= "</tr>";
-    $cont++;
+foreach ($alunos as $key => $value) {
+    $lacoNome .= "<tr>";
+    $lacoNome .= "<td>{$value['nome']}</td>";
+    $lacoNome .= "<td>{$value['matricula']}</td>";
+    $lacoNome .= "<td>{$value['email']}</td>";
+    $lacoNome .= "</tr>";
 }
+foreach ($turmas as $key => $value) {
+    $lacoTurma .= "<tr>";
+    $lacoTurma .= "<td>{$value['ano']}</td>";
+    $lacoTurma .= "<td>{$value['semestre']}</td>";
+    $lacoTurma .= "</tr>";
+}
+foreach ($consultabd as $key => $value) {
+    $lacoDisciplina .= "<tr>";
+    $lacoDisciplina .= "<td>{$value['conteudo']}</td>";
+    $lacoDisciplina .= "<td>{$value['nota']}</td>";
+    $lacoDisciplina .= "<td>{$value['dataInicio']}</td>";
+    $lacoDisciplina .= "</tr>";
+}
+$pagina = "
+<div class='row' >
+    <div class='col-sm-12'>
+        <div class='card-box'>
+            <center><h4 class='page-title' color=black>Historico Aluno</h4></center>
+      <br>
+      <br>
+      <font  color=blue size=3>Dados Aluno </font>
+      <br>
+      <br>
+     
+";
+$pagina.="
+      <TABLE BORDER=1 width='1000' > <!-- Tabela Herder  -->
+          <TR> 
+              <TD width='100' bgcolor='#edf5ff' >Nome:</TD> 
+              <TD width='100' bgcolor='#edf5ff' >Matricula </TD>
+              <TD width='100' bgcolor='#edf5ff' > Email: </TD>
+          </TR> 
+          $lacoNome
+     </TABLE>
+        <br>
+       <font  color=blue size=3>Dados do Curso </font>
+      <br>
+      <br>
+     
+      <TABLE BORDER=1 width='1000'> <!-- Tabela Herder  -->
+          <TR> 
+              <TD width='100' bgcolor='#edf5ff'>Ano </TD>
+              <TD width='100' bgcolor='#edf5ff' >Semestre</TD> 
+          </TR> 
+            $lacoTurma
+      </TABLE>
+         <br>
 
-$table = "
-    <table id='playlistTable'>
-  <thead>
-    <tr>
-      <th><strong>Nº</strong></th>
-      <th><strong>Atividade</strong></th>
-      <th><strong>Nota</strong></th>
-      <th><strong>Data</strong></th>
-    </tr>
-  </thead>
+         <font  color=blue size=3>Dados do Disciplinas </font>
+      <br>
+      <br>
+      <TABLE BORDER=1 width='800'> <!-- Tabela Herder  -->
+          <TR> 
+              <TD width='100' bgcolor='#edf5ff' >Avaliação</TD> 
+              <TD width='100' bgcolor='#edf5ff'>Nota </TD>
+              <TD width='100' bgcolor='#edf5ff'>Data</TD>
+          </TR> 
+          $lacoDisciplina
+           
 
-  <tbody>
-    $laco   
-  </tbody>
-</table>
-    ";
-
-$html = "
-	 <fieldset>
-	 	
-  <caption><h1>Histórico</h1></caption>
-	 	<p>Aluno: <strong> $nome </strong></p>
-	 	<p>Matricula: <strong>$matricula</strong></p>
-	 	<p>Ano: <strong>$ano<strong></p>
-	 	<p>Turma: <strong>$turma<strong></p>
-	 	<br>
-		$table
-		<br>
-		<br>
-		<br>
-		<caption><p class='center sub-titulo'>          .........................................................................</p>
-	 	<p class='center '><strong>Coordenador</strong></p></caption>
-	 </fieldset>";
+      </TABLE>
+        </div>
+    </div>
+</div>
+   </div>
+</div>
+</div>
+"; 
+$arquivo = "historico.pdf";
 
 $mpdf = new mPDF();
-$mpdf->SetDisplayMode('fullpage');
-$css = file_get_contents("./assets/css/estilo.css");
-$mpdf->WriteHTML($css, 1);
-$mpdf->WriteHTML($html);
-$mpdf->Output();
+$mpdf->WriteHTML($pagina);
 
-exit;
+$mpdf->Output($arquivo, 'I');
+?>
