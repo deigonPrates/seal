a5ee67ded479a6584ab82e80b7cf8a81627debb4
Deigon <deigon_prates@hotmail.com>
2017-06-11 19:24:42 -0300

    adicionando novas funcionalidades ao codmirror

--- codMirrrorScripts.php
+++ codMirrrorScripts.php
@@ -1,10 +1,10 @@
 <script>
-  var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+  var editor = CodeMirror.fromTextArea(document.getElementById("code1"), {
     lineNumbers: true,
     styleActiveLine: true,
     matchBrackets: true
   });
-  var input = document.getElementById("select");
+  var input = document.getElementById("selecte");
   function selectTheme() {
     var theme = input.options[input.selectedIndex].textContent;
     editor.setOption("theme", theme);
@@ -23,12 +23,36 @@
   });
 </script>
 <script>
-  var editor = CodeMirror.fromTextArea(document.getElementById("code1"), {
+  var editor = CodeMirror.fromTextArea(document.getElementById("code2"), {
     lineNumbers: true,
     styleActiveLine: true,
     matchBrackets: true
   });
-  var input = document.getElementById("select1");
+  var input = document.getElementById("selects");
+  function selectTheme() {
+    var theme = input.options[input.selectedIndex].textContent;
+    editor.setOption("theme", theme);
+    location.hash = "#" + theme;
+  }
+  var choice = (location.hash && location.hash.slice(1)) ||
+               (document.location.search &&
+                decodeURIComponent(document.location.search.slice(1)));
+  if (choice) {
+    input.value = choice;
+    editor.setOption("theme", choice);
+  }
+  CodeMirror.on(window, "hashchange", function() {
+    var theme = location.hash.slice(1);
+    if (theme) { input.value = theme; selectTheme(); }
+  });
+</script>
+<script>
+  var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+    lineNumbers: true,
+    styleActiveLine: true,
+    matchBrackets: true
+  });
+  var input = document.getElementById("select");
   function selectTheme() {
     var theme = input.options[input.selectedIndex].textContent;
     editor.setOption("theme", theme);
