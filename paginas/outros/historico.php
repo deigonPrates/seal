d9ffd41ca4590c3f60322c4a013a68bb1b51cf1f
Deigon <deigon_prates@hotmail.com>
2017-06-24 22:09:42 -0300

    reformulando completamente o historico para o padrão do if

--- paginas/outros/historico.php
+++ paginas/outros/historico.php
@@ -9,6 +9,7 @@ include_once("./classes/conexao.class.php");
 
 $conexao = new Conexao();
 $con = $conexao->BDAbreConexao();
+$bdID = (int)$conexao->BDRetornaID($_SESSION['matricula']);
 
 $alunos = $conexao->BDSeleciona('alunos', 'alunos.nome, alunos.email,alunos.matricula', "WHERE(id = 1)");
 $turmas = $conexao->BDSeleciona('turmas', 'turmas.ano, turmas.semestre', "WHERE(id = 1)");
@@ -17,73 +18,59 @@ $consultabd = $conexao->BDSQL("SELECT sum(questoes.valor) as nota, atividades.co
         . "JOIN atividades on atividades.id = alunos_atividades.avaliacao_id "
         . "JOIN questoes on atividades.id = questoes.atividade_id "
         . "join respostas on questoes.id = respostas.questao_id "
-        . "WHERE(respostas.resultado = 1 and alunos.id = 1) "
+        . "WHERE(respostas.resultado = 1 and alunos.id = $bdID) "
         . "GROUP by atividades.conteudo, atividades.dataInicio");
 $conexao->BDFecharConexao($con);
 
 foreach ($alunos as $key => $value) {
     $lacoNome .= "<tr>";
-    $lacoNome .= "<td>{$value['nome']}</td>";
-    $lacoNome .= "<td>{$value['matricula']}</td>";
-    $lacoNome .= "<td>{$value['email']}</td>";
+    $lacoNome .= "<td>Nome: {$value['nome']}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+    $lacoNome .= "Matricula: {$value['matricula']}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+    $lacoNome .= "Email: {$value['email']}</td>";
     $lacoNome .= "</tr>";
 }
 foreach ($turmas as $key => $value) {
     $lacoTurma .= "<tr>";
-    $lacoTurma .= "<td>{$value['ano']}</td>";
-    $lacoTurma .= "<td>{$value['semestre']}</td>";
+    $lacoTurma .= "<td><strong>Graduação:&nbsp;&nbsp;&nbsp;</strong> Curso Superior de Tecnologia em Análise e Desenvolvimento de Sistemas <br>";
+    $lacoTurma .= "<strong>Ano:&nbsp;&nbsp;&nbsp;</strong>{$value['ano']}".'.';
+    $lacoTurma .= "{$value['semestre']}</td>";
     $lacoTurma .= "</tr>";
 }
 foreach ($consultabd as $key => $value) {
     $lacoDisciplina .= "<tr>";
-    $lacoDisciplina .= "<td>{$value['conteudo']}</td>";
-    $lacoDisciplina .= "<td>{$value['nota']}</td>";
-    $lacoDisciplina .= "<td>{$value['dataInicio']}</td>";
+    $lacoDisciplina .= "<td><center>{$value['conteudo']}</center></td>";
+    $lacoDisciplina .= "<td><center>{$value['nota']}</center></td>";
+    $lacoDisciplina .= "<td><center>{$value['dataInicio']}</center></td>";
     $lacoDisciplina .= "</tr>";
 }
 $pagina = "
 <div class='row' >
     <div class='col-sm-12'>
         <div class='card-box'>
-            <center><h4 class='page-title' color=black>Historico Aluno</h4></center>
-      <br>
-      <br>
-      <font  color=blue size=3>Dados Aluno </font>
-      <br>
+        <center><img src='./assets/images/headerHistorico.png'></center>
       <br>
      
 ";
 $pagina.="
-      <TABLE BORDER=1 width='1000' > <!-- Tabela Herder  -->
+      <TABLE width='1000' > <!-- Tabela Herder  -->
           <TR> 
-              <TD width='100' bgcolor='#edf5ff' >Nome:</TD> 
-              <TD width='100' bgcolor='#edf5ff' >Matricula </TD>
-              <TD width='100' bgcolor='#edf5ff' > Email: </TD>
+              <TD width='100' bgcolor='#A9A9A9'><center>IDENTIFICAÇÃO</center></TD>
           </TR> 
           $lacoNome
      </TABLE>
         <br>
-       <font  color=blue size=3>Dados do Curso </font>
-      <br>
-      <br>
-     
-      <TABLE BORDER=1 width='1000'> <!-- Tabela Herder  -->
+      <TABLE width='1000'> <!-- Tabela Herder  -->
           <TR> 
-              <TD width='100' bgcolor='#edf5ff'>Ano </TD>
-              <TD width='100' bgcolor='#edf5ff' >Semestre</TD> 
+             <TD width='100' bgcolor='#A9A9A9'><center>CURSO</center></TD>
           </TR> 
             $lacoTurma
       </TABLE>
          <br>
-
-         <font  color=blue size=3>Dados do Disciplinas </font>
-      <br>
-      <br>
-      <TABLE BORDER=1 width='800'> <!-- Tabela Herder  -->
+      <TABLE width='1000'> <!-- Tabela Herder  -->
           <TR> 
-              <TD width='100' bgcolor='#edf5ff' >Avaliação</TD> 
-              <TD width='100' bgcolor='#edf5ff'>Nota </TD>
-              <TD width='100' bgcolor='#edf5ff'>Data</TD>
+              <TD width='100' bgcolor='#A9A9A9' ><center>AVALIAÇÃO</center></TD> 
+              <TD width='100' bgcolor='#A9A9A9'><center>NOTA</center></TD>
+              <TD width='100' bgcolor='#A9A9A9'><center>DATA</center></TD>
           </TR> 
           $lacoDisciplina
            
@@ -96,7 +83,7 @@ $pagina.="
 </div>
 </div>
 "; 
-$arquivo = "historico.pdf";
+$arquivo = "histórico.pdf";
 
 $mpdf = new mPDF();
 $mpdf->WriteHTML($pagina);
